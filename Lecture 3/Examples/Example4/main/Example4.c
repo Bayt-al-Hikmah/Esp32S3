2d7bf6c4fb0c18f249857fc6986039eb05133e9c
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2026-06-14 14:16:43 +0100

    Add GPIO control in app_main function
    Implement GPIO control for light state toggling based on input pin.

--- Lecture 3/Examples/Example4/main/Example4.c
+++ Lecture 3/Examples/Example4/main/Example4.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void app_main(void)
-{
+bool lightState = false;
 
+void app_main(void){
+
+    gpio_reset_pin(1);
+    gpio_reset_pin(10);
+    
+
+    gpio_set_direction(1, GPIO_MODE_OUTPUT); 
+    gpio_set_direction(10, GPIO_MODE_INPUT);  
+
+    gpio_set_pull_mode(10,GPIO_PULLDOWN_ONLY);
+
+    while (1) {
+        if(gpio_get_level(10) == 0){
+            lightState ^=1;
+            gpio_set_level(1,lightState);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        } 
+        
+    }
 }
